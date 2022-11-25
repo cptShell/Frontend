105e8fc516a23535868b8f33021d0fa660755a28
Anton Zhuravlev <abramoff.job@gmail.com>
2022-11-25 17:17:24 +0300

    registration

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Login({changeAuth}) {
+	const [email, setEmail] = useState(""),
+		[password, setPassword] = useState("")
+
 	return (
 
 
-		<div className="block__authorization authorization-block">
+		<form className="block__authorization authorization-block">
 
-			<input type="text" className="authorization-block__google-input"/>
+			<input type="text" className="authorization-block__google-input form__input"/>
 
 			<span className="authorization-block__text">or</span>
 
-			<input type="mail" className="authorization-block__mail" placeholder="Email"/>
-			<input type="password" className="authorization-block__password" placeholder="Password"/>
+			<input
+				value={email} onChange={(e) => setEmail(e.target.value)}
+				type="email" className="authorization-block__mail form__input" placeholder="Email"/>
+			<input
+				value={password} onChange={(e) => setPassword(e.target.value)}
+				type="password" className="authorization-block__password form__input" placeholder="Password"/>
 			<button className="authorization-block__create-account">Log in</button>
 
 			<p className="authorization-block__log-in">No account?
-                <span className="authorization-block__link" onClick={changeAuth}>Create one</span>
-            </p>
+				<span className="authorization-block__link" onClick={changeAuth}>Create one</span>
+			</p>
 
-		</div>
+		</form>
 
 
 	);
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,25 +1,110 @@
-import React from "react";
+import React, {useState} from "react";
+import {useForm} from "react-hook-form";
 
-function Registration({ changeAuth }) {
-  return (
+function Registration({changeAuth}) {
+
+	const {
+		register,
+		formState: {
+			errors,
+		},
+		handleSubmit,
+	} = useForm({
+		mode: "onBlur"
+	})
+
+	const onSubmit = (data, e) => {
+		e.preventDefault()
+		fetch("http://127.0.0.1:8000/users/user/", {
+			mode: 'no-cors',
+			method: "POST",
+			body: JSON.stringify(data),
+		})
+			.then(data => data.json())
+			.then(r => console.log(r))
+		console.log(JSON.stringify(data))
+	}
+
+	return (
+
+		<form className="block__authorization authorization-block"
+		      onSubmit={handleSubmit(onSubmit)}
+		      noValidate
+		>
+
+			<div>
+				<input type="text" className="authorization-block__google-input form__input "/>
+			</div>
+
+			<span className="authorization-block__text">or</span>
+			<div className={"relative"}>
+				<input
+					{...register("email", {
+						required: "Обязательное поле",
+						minLength: {
+							value: 5,
+							message: "Минимальная длина 5 символов"
+						},
+						maxLength: {
+							value: 35,
+							message: "Максимальная длина 35 символов"
+						},
+						pattern: {
+							value: /\w+@\w+\.\w+/,
+							message: "Некорректное поле"
+						},
+
+					})}
+
+					type="email" className={errors?.email ? "authorization-block__password form__input relative" +
+					" border-red-500" : "authorization-block__password form__input relative"} placeholder="Email"
+
+				/>
+				{errors.email &&
+					<p className={"absolute bottom-[-24px] left-1 text-md text-red-500 font-medium"}>{errors?.email?.message || "Error!"}</p>}
+			</div>
+
+			<div className={"relative"}>
+				<input
+					{...register("password", {
+						required: "Обязателное поле",
+						minLength: {
+							value: 4,
+							message: "Минимальная длина 4 символов"
+						},
+						maxLength: {
+							value: 20,
+							message: "Максимальная длина 20 символов"
+						},
+						pattern: {
+							value: /\w/,
+							message: "Некорректное поле"
+						}
+
+					})}
+
+					type="password" className={errors?.password ? "authorization-block__password form__input relative" +
+					" border-red-500" : "authorization-block__password form__input relative"}
+					placeholder="Password"
+
+				/>
+				{errors.password &&
+					<p className={"absolute bottom-[-24px] left-1 text-md text-red-500 font-medium"}>{errors?.password?.message || "Error!"}</p>}
+			</div>
 
 
-    <div className="block__authorization authorization-block">
+			<button className="authorization-block__create-account"
 
-      <input type="text" className="authorization-block__google-input" />
+			>Create account
+			</button>
 
-      <span className="authorization-block__text">or</span>
+			<p className="authorization-block__log-in">Already have an account? <span
+				className="authorization-block__link" onClick={changeAuth}>Log in</span></p>
 
-      <input type="mail" className="authorization-block__mail" placeholder="Email" />
-      <input type="password" className="authorization-block__password" placeholder="Password" />
-      <button className="authorization-block__create-account">Create account</button>
-
-      <p className="authorization-block__log-in">Already have an account? <span className="authorization-block__link" onClick={changeAuth}>Log in</span></p>
-
-    </div>
+		</form>
 
 
-  );
+	);
 }
 
 export default Registration;
